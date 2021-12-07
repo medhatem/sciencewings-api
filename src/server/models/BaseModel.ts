8c128858e2f242595b8ced86b27e0151a5278cbf
abdesselam <xpack94@outlook.com>
2021-12-07 03:01:15 -0500

    add timestamps in baseModel

--- src/server/models/BaseModel.ts
+++ src/server/models/BaseModel.ts
@@ -1,5 +1,13 @@
-export class BaseModel<T> {
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export class BaseModel<T = any> {
   static getInstance(): void {
     throw new Error('The base model class cannot be instanciated and needs to be overriden!');
   }
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+  public created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)', onUpdate: 'CURRENT_TIMESTAMP(6)' })
+  public updated_at: Date;
 }
