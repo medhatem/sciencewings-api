1f31ebf3b8c52d8250a6d7575d63577bfd950ba5
splimter <merah.soheyb@gmail.com>
2022-02-21 17:07:57 +0100

    rm comment

--- src/server/utils/utilities.ts
+++ src/server/utils/utilities.ts
@@ -4,24 +4,6 @@ const wait = async (time: number) => {
   });
 };
 
-// export const applyToAll = async (array: any, cb: any, isAsync = true, pauseTime = 0) => {
-//   if (isAsync) {
-//     return await Promise.all(
-//       array.map(async (el: any, index: number) => {
-//         await cb(el, index);
-//         await pause(pauseTime);
-//       }),
-//     );
-//   } else {
-//     const arr = [];
-//     for (let i = 0; i < array.length; i++) {
-//       arr.push(await cb(array[i], i));
-//     }
-
-//     return arr;
-//   }
-// };
-
 export const applyToAll = async <T, G>(
   list: T[],
   callback: (element: T, index?: number) => Promise<G>,
