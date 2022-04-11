597862ea3577a9c6f864de58417751ecfaa44864
splimter <merah.soheyb@gmail.com>
2022-04-11 14:33:41 +0100

    cleaning

--- src/server/modules/resources/interfaces/IResourceService.ts
+++ src/server/modules/resources/interfaces/IResourceService.ts
@@ -1,8 +1,3 @@
 import { IBaseService } from '@/modules/base/interfaces/IBaseService';
-import { Result } from '@/utils/Result';
 
-export abstract class IResourceService extends IBaseService<any> {
-  getResourcesSettingsGeneralStatus: (resourceId: number) => Promise<Result<any>>;
-  getResourceSettingsGeneralVisbility: (resourceId: number) => Promise<Result<any>>;
-  getResourceSettingsGeneralProperties: (resourceId: number) => Promise<Result<any>>;
-}
+export abstract class IResourceService extends IBaseService<any> {}
