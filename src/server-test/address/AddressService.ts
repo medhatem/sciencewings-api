05d768558e726f68fa21a3d3f70cb63febbf3cfb
splimter <merah.soheyb@gmail.com>
2022-04-28 14:15:56 +0100

    test

---
+++ src/server-test/address/AddressService.ts
@@ -0,0 +1,34 @@
+import intern from 'intern';
+import { stub, restore } from 'sinon';
+const { suite, test } = intern.getPlugin('interface.tdd');
+const { expect } = intern.getPlugin('chai');
+import { afterEach, beforeEach } from 'intern/lib/interfaces/tdd';
+import { container } from '@/di';
+import { Configuration } from '@/configuration/Configuration';
+import { Logger } from '@/utils/Logger';
+import { AddressService } from '@/modules/address/services/AddressService';
+
+suite(__filename.substring(__filename.indexOf('/server-test') + '/server-test/'.length), (): void => {
+  beforeEach(() => {
+    const _container = stub(container, 'get');
+    _container.withArgs(Configuration).returns({
+      getConfiguration: stub(),
+      currentENV: 'test',
+    });
+    _container.withArgs(Logger).returns({
+      setup: stub(),
+      info: stub(),
+      error: stub(),
+      warn: stub(),
+    });
+  });
+
+  afterEach(() => {
+    restore();
+  });
+
+  test('should create the right instance', () => {
+    const instance = AddressService.getInstance();
+    expect(instance instanceof AddressService);
+  });
+});
