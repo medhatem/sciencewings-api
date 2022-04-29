502802e1c632692d747bea0a1be348baacfdf902
bassetsadok <salah.sadok67@gmail.com>
2022-04-29 01:29:23 +0100

    create the basic functionalities for statusHistory

---
+++ src/server/modules/resources/models/ResourceStatusHistory.ts
@@ -0,0 +1,39 @@
+import { container, provide } from '@/di';
+import { BaseModel } from '@/modules/base';
+import { Member } from '@/modules/hr';
+import { Entity, OneToOne, PrimaryKey, Property } from '@mikro-orm/core';
+import { Resource } from './Resource';
+import { ResourceStatus } from './ResourceStatus';
+
+@provide()
+@Entity()
+export class ResourceStatusHistory extends BaseModel<ResourceStatusHistory> {
+  constructor() {
+    super();
+  }
+
+  static getInstance(): ResourceStatusHistory {
+    return container.get(ResourceStatusHistory);
+  }
+
+  @PrimaryKey()
+  id: number;
+
+  @OneToOne({
+    entity: () => Resource,
+    nullable: true,
+  })
+  resource: Resource;
+
+  @OneToOne({
+    entity: () => Member,
+    nullable: true,
+  })
+  member: Member;
+
+  @OneToOne({ entity: () => ResourceStatus, onDelete: 'set null', nullable: true })
+  resourceStatus: ResourceStatus;
+
+  @Property()
+  statusDescription: string;
+}
