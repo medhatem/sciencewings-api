1582ee289f640e7ae03365b8163a6c39c954aee8
splimter <merah.soheyb@gmail.com>
2022-05-01 18:32:01 +0100

    end service test

--- src/server-test/phones/PhoneService.ts
+++ src/server-test/phones/PhoneService.ts
@@ -1,5 +1,5 @@
 import intern from 'intern';
-import { stub, restore } from 'sinon';
+import { stub, restore, createStubInstance, SinonStubbedInstance } from 'sinon';
 const { suite, test } = intern.getPlugin('interface.tdd');
 const { expect } = intern.getPlugin('chai');
 import { afterEach, beforeEach } from 'intern/lib/interfaces/tdd';
@@ -7,9 +7,13 @@ import { container } from '@/di';
 import { Configuration } from '@/configuration/Configuration';
 import { Logger } from '@/utils/Logger';
 import { PhoneService } from '@/modules/phones/services/PhoneService';
+import { PhoneDao } from '@/modules/phones/daos/PhoneDAO';
+import { BaseService } from '@/modules/base';
 
 suite(__filename.substring(__filename.indexOf('/server-test') + '/server-test/'.length), (): void => {
+  let phoneDAO: SinonStubbedInstance<PhoneDao>;
   beforeEach(() => {
+    phoneDAO = createStubInstance(PhoneDao);
     const _container = stub(container, 'get');
     _container.withArgs(Configuration).returns({
       getConfiguration: stub(),
@@ -21,6 +25,9 @@ suite(__filename.substring(__filename.indexOf('/server-test') + '/server-test/'.
       error: stub(),
       warn: stub(),
     });
+    _container.withArgs(PhoneService).returns(new PhoneService(phoneDAO));
+    stub(BaseService.prototype, 'wrapEntity').returns({});
+    (phoneDAO.repository as any) = { persist: stub() };
   });
 
   afterEach(() => {
@@ -30,5 +37,21 @@ suite(__filename.substring(__filename.indexOf('/server-test') + '/server-test/'.
   test('should create the right instance', () => {
     const instance = PhoneService.getInstance();
     expect(instance instanceof PhoneService);
+  });
+
+  suite('createBulkPhoneForUser', () => {
+    test('Should success on creation', async () => {
+      const result = await container.get(PhoneService).createBulkPhoneForUser([] as any, {} as any);
+      expect(result.isSuccess).to.be.true;
+      expect(result.getValue()).to.equal(200);
+    });
+  });
+
+  suite('createBulkPhoneForOrganization', () => {
+    test('Should success on creation', async () => {
+      const result = await container.get(PhoneService).createBulkPhoneForOrganization([] as any, {} as any);
+      expect(result.isSuccess).to.be.true;
+      expect(result.getValue()).to.equal(200);
+    });
   });
 });
