b51ac0b51e438e72612a6d7d8e2752809196c7b3
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2022-06-16 14:20:27 +0100

    create the getMembershipRoute

--- src/server/modules/hr/interfaces/IMemberService.ts
+++ src/server/modules/hr/interfaces/IMemberService.ts
@@ -1,7 +1,9 @@
 import { IBaseService } from '@/modules/base/interfaces/IBaseService';
 import { Result } from '@/utils/Result';
+import { Member } from '../models';
 
 export abstract class IMemberService extends IBaseService<any> {
   inviteUserByEmail: (email: string, orgId: number) => Promise<Result<number>>;
   resendInvite: (id: number, orgId: number) => Promise<Result<number>>;
+  getUserMemberships: (userId: number) => Promise<Result<Member[]>>;
 }
