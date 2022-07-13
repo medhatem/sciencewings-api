e9015cef03d23c74efa71920108504d636096e26
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2022-07-13 23:38:48 +0100

    infra model

--- src/server/modules/infrastructure/models/Infrastructure.ts
+++ src/server/modules/infrastructure/models/Infrastructure.ts
@@ -1,7 +1,9 @@
-import { Entity, PrimaryKey } from '@mikro-orm/core';
+import { Collection, Entity, ManyToOne, OneToMany, OneToOne, PrimaryKey, Property, Unique } from '@mikro-orm/core';
 import { container, provide } from '@/di/index';
-
 import { BaseModel } from '@/modules/base/models/BaseModel';
+import { Organization } from '@/modules/organizations/models/Organization';
+import { User } from '@/modules/users/models/User';
+import { Resource } from '@/modules/resources/models/Resource';
 
 @provide()
 @Entity()
@@ -16,4 +18,26 @@ export class Infrastructure extends BaseModel<Infrastructure> {
 
   @PrimaryKey()
   id?: number;
+
+  @Property()
+  name!: string;
+
+  @Property({ nullable: true })
+  description!: string;
+
+  @Unique({ key: 'infrastructure_key_uniq' })
+  @Property()
+  key!: string;
+
+  @OneToOne({
+    entity: () => User,
+    unique: false,
+  })
+  public responsable!: User;
+
+  @ManyToOne({ entity: () => Organization, onDelete: 'set null', nullable: true })
+  organization?: Organization;
+
+  @OneToMany({ entity: () => Resource, mappedBy: (res) => res.infrastructure, nullable: true })
+  resources? = new Collection<Resource>(this);
 }
