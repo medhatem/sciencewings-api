dd1addd9bdc3c05b83737cf2d9882ee31ef015ed
abdesselam <xpack94@outlook.com>
2022-07-21 03:30:33 -0400

    fix address errors

--- src/server/modules/address/interfaces/IAddressService.ts
+++ src/server/modules/address/interfaces/IAddressService.ts
@@ -1,8 +1,7 @@
 import { Address } from '@/modules/address/models/Address';
 import { AddressRO } from '@/modules/address/routes/AddressRO';
 import { IBaseService } from '@/modules/base/interfaces/IBaseService';
-import { Result } from '@/utils/Result';
 
 export abstract class IAddressService extends IBaseService<any> {
-  createAddress: (payload: AddressRO) => Promise<Result<Address>>;
+  createAddress: (payload: AddressRO) => Promise<Address>;
 }
