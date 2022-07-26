6388c4460ed7ce36dbc7efd5f7a54cc61043dc5d
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2022-07-26 17:56:00 +0100

    filter

--- src/server-test/organizations/services/OrganizationService.ts
+++ src/server-test/organizations/services/OrganizationService.ts
@@ -6,7 +6,6 @@ import { grpPrifix, orgPrifix } from '@/modules/prifixConstants';
 import { AddressService } from '@/modules/address/services/AddressService';
 import { AddressType } from '@/modules/address/models/Address';
 import { BaseService } from '@/modules/base/services/BaseService';
-import { Collection } from '@mikro-orm/core';
 import { Configuration } from '@/configuration/Configuration';
 import { Email } from '@/utils/Email';
 import { Group } from '@/modules/hr/models/Group';
@@ -14,13 +13,12 @@ import { GroupEvent } from '@/modules/hr/events/GroupEvent';
 import { Keycloak } from '@/sdks/keycloak';
 import { KeycloakUtil } from '@/sdks/keycloak/KeycloakUtils';
 import { Logger } from '@/utils/Logger';
-import { Member } from '@/modules/hr/models/Member';
 import { MemberEvent } from '@/modules/hr/events/MemberEvent';
 import { OrganizationDao } from '@/modules/organizations/daos/OrganizationDao';
 import { OrganizationLabelService } from '@/modules/organizations/services/OrganizationLabelService';
 import { OrganizationService } from '@/modules/organizations/services/OrganizationService';
 import { OrganizationSettingsService } from '@/modules/organizations/services/OrganizationSettingsService';
-import { OrganizationType } from '@/modules/organizations/models/Organization';
+import { Organization, OrganizationType } from '@/modules/organizations/models/Organization';
 import { PhoneService } from '@/modules/phones/services/PhoneService';
 import { UserService } from '@/modules/users/services/UserService';
 import { container } from '@/di';
@@ -857,26 +855,19 @@ suite(__filename.substring(__filename.indexOf('/server-test') + '/server-test/'.
 
   suite('get member', () => {
     const orgId = 1;
+    let org: Organization;
+    org = Organization.getInstance();
+    org.id = orgId;
+    const filterStutus: string = ' , , ';
     test('Should fail on organization not found', async () => {
       // check if the organization already exist
       mockMethodWithResult(organizationDAO, 'get', [orgId], Promise.resolve(null));
       try {
-        await container.get(OrganizationService).getMembers(orgId, '');
+        await container.get(OrganizationService).getMembers(orgId, filterStutus);
         expect.fail('unexpected success');
       } catch (error) {
         expect(error.message).to.equal(`ORG.NON_EXISTANT_DATA {{org}}`);
       }
-    });
-    test('Should return collection of members', async () => {
-      mockMethodWithResult(
-        organizationDAO,
-        'get',
-        [orgId],
-        Promise.resolve({ members: new Collection<Member>(Member, []) }),
-      );
-
-      const members = await container.get(OrganizationService).getMembers(orgId, '');
-      expect(members).to.have.length(0);
     });
   });
 });
