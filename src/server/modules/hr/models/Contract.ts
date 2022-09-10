a97191a2d4f1f83c823a0f12e3f1641e6b36ed55
bassetsadok <salah.sadok67@gmail.com>
2022-09-10 09:39:07 +0100

    job level fix

--- src/server/modules/hr/models/Contract.ts
+++ src/server/modules/hr/models/Contract.ts
@@ -7,7 +7,7 @@ import { ResourceCalendar } from '@/modules/resources/models/ResourceCalendar';
 import { BaseModel } from '@/modules/base/models/BaseModel';
 
 export enum JobLevel {
-  INTERN = 'Intern',
+  INTERNSHIP = 'Internship',
   JUNIOR = 'Junior',
   MID = 'Midlle',
   MID_SENIOR = 'Mid-senior',
