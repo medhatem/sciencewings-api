bd6e6c864e3a8b699e645a83e9677016d4c45fd5
abdesselam <xpack94@outlook.com>
2022-09-14 06:17:55 -0400

    add multiple views for the calendar

--- src/server/modules/reservation/models/Reservation.ts
+++ src/server/modules/reservation/models/Reservation.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne, OneToOne, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core';
 import { container, provide } from '@/di/index';
 
 import { BaseModel } from '@/modules/base/models/BaseModel';
@@ -31,6 +31,6 @@ export class Reservation extends BaseModel<Reservation> {
   @ManyToOne({ entity: () => Calendar, onDelete: 'set null' })
   resourceCalendar: Calendar;
 
-  @OneToOne({ entity: () => User, nullable: false })
+  @ManyToOne({ entity: () => User, onDelete: 'set null', nullable: true })
   user: User;
 }
