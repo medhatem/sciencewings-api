c8cf60377428d6c79dc6779ba60fc031c76ead32
bassetsadok <salah.sadok67@gmail.com>
2022-09-23 21:58:13 +0100

    fix

--- src/server/modules/phones/services/PhoneService.ts
+++ src/server/modules/phones/services/PhoneService.ts
@@ -2,7 +2,6 @@ import { container, provideSingleton } from '@/di/index';
 
 import { BaseService } from '@/modules/base/services/BaseService';
 import { IPhoneService } from '@/modules/phones/interfaces/IPhoneService';
-import { Organization } from '@/modules/organizations/models/Organization';
 import { Phone } from '@/modules/phones/models/Phone';
 import { PhoneDao } from '@/modules/phones/daos/PhoneDAO';
 import { PhoneRO } from '@/modules/phones/routes/PhoneRO';
@@ -43,7 +42,7 @@ export class PhoneService extends BaseService<Phone> implements IPhoneService {
   }
 
   @log()
-  async createBulkPhoneForOrganization(payload: PhoneRO[], entity: Organization): Promise<void> {
+  async createBulkPhoneForOrganization(payload: PhoneRO[]): Promise<void> {
     if (payload) {
       const phones = await Promise.all(
         payload.map(async (phone) => {
