37a9edd6dad907ab7957cc39fe3cd06ea2cfb0a9
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2022-11-05 21:03:20 +0100

    create the needed ck permissions in org module

--- src/server/modules/organizations/services/OrganizationService.ts
+++ src/server/modules/organizations/services/OrganizationService.ts
@@ -41,6 +41,8 @@ import { IInfrastructureService } from '@/modules/infrastructure/interfaces/IInf
 import { IMemberService } from '@/modules/hr/interfaces/IMemberService';
 import { paginate } from '@/utils/utilities';
 import { MembersList } from '@/types/types';
+import { Permission } from '@/modules/permissions/models/permission';
+import { IPermissionService } from '@/modules/permissions/interfaces/IPermissionService';
 
 @provideSingleton(IOrganizationService)
 export class OrganizationService extends BaseService<Organization> implements IOrganizationService {
@@ -57,6 +59,7 @@ export class OrganizationService extends BaseService<Organization> implements IO
     public emailService: Email,
     public keycloak: Keycloak,
     public keycloakUtils: KeycloakUtil,
+    public permissionService: IPermissionService,
   ) {
     super(dao);
     //this.infraService = infraService;
@@ -225,6 +228,24 @@ export class OrganizationService extends BaseService<Organization> implements IO
     defaultInfrastructure.responsible = responsable;
     defaultInfrastructure.organization = organization;
     await this.infraService.create(defaultInfrastructure);
+
+    // create the necessary CK Permissions
+    let KcPERmissions: any[] = [];
+    const BDPermmissions = (await this.permissionService.getByCriteria(
+      { module: 'organization', operationDB: 'create' },
+      FETCH_STRATEGY.ALL,
+    )) as Permission[];
+    if (BDPermmissions) {
+      for (const permission of BDPermmissions) {
+        KcPERmissions.push(permission.name);
+      }
+
+      for (const KcPermission of KcPERmissions) {
+        this.keycloakUtils.createRealmRole(`${organization.kcid}-${KcPermission}`);
+        const currentRole = await this.keycloakUtils.findRoleByName(`${organization.kcid}-${KcPermission}`);
+        this.keycloakUtils.groupRoleMap(adminGroup, currentRole);
+      }
+    }
 
     return organization.id;
   }
@@ -433,6 +454,20 @@ export class OrganizationService extends BaseService<Organization> implements IO
     await this.keycloakUtils.deleteGroup(fetchedorganization.kcid);
 
     await this.dao.remove(fetchedorganization);
+
+    //clean the org permision
+    let KcPERmissions: string[];
+    const BDPermmissions = (await this.permissionService.getByCriteria(
+      { module: 'organization', operationDB: 'create' },
+      FETCH_STRATEGY.ALL,
+    )) as Permission[];
+    for (const permission of BDPermmissions) {
+      KcPERmissions.push(permission.name);
+    }
+
+    for (const KcPermission of KcPERmissions) {
+      this.keycloakUtils.deleteRealmRole(`${fetchedorganization.kcid}-${KcPermission}`);
+    }
 
     return organizationId;
   }
