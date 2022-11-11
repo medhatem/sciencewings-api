940fdd463fb681bf95328b62a1d1e2c6a1e30798
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2022-11-11 17:48:55 +0100

    permissions seedings

---
+++ src/server/seeders/permissionseeds.ts
@@ -0,0 +1,6 @@
+export const permissionSeeds = [
+  { name: 'view-org', module: 'organization', operationDB: 'create' },
+  { name: 'update-org', module: 'organization', operationDB: 'create' },
+  { name: 'delete-org', module: 'organization', operationDB: 'create' },
+  { name: 'create-org', module: 'organization', operationDB: 'create' },
+];
