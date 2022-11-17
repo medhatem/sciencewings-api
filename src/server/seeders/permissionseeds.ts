1b6fe98b5f7312809b0a5b6310c1fa953c06e160
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2022-11-17 14:12:09 +0100

    project routes permissions

--- src/server/seeders/permissionseeds.ts
+++ src/server/seeders/permissionseeds.ts
@@ -9,4 +9,14 @@ export const permissionSeeds = [
   { name: 'view-organization-members', module: 'organization', operationDB: 'create' },
   { name: 'view-organization-settings', module: 'organization', operationDB: 'create' },
   { name: 'create-resource', module: 'organization', operationDB: 'create' },
+  { name: 'view-organization-projects', module: 'organization', operationDB: 'create' },
+  { name: 'create-project', module: 'organization', operationDB: 'create' },
+  { name: 'view-organization-project-list', module: 'organization', operationDB: 'create' },
+
+  // project permissions
+  { name: '{orgId}-view-organization-project', module: 'organization', operationDB: 'create' },
+  { name: '{orgId}-update-project', module: 'organization', operationDB: 'create' },
+  { name: 'create-project-members', module: 'organization', operationDB: 'create' },
+  { name: 'view-project-participants', module: 'organization', operationDB: 'create' },
+  { name: 'update-project-participants', module: 'organization', operationDB: 'create' },
 ];
