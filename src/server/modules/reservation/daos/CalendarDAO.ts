e0e32682c873bc23e166ceeffddbedabb2550508
kouadri habbaze yassamina <kyassamina@sciencewings.com>
2023-02-08 00:52:20 +0100

    investigate dependancy injection

--- src/server/modules/reservation/daos/CalendarDAO.ts
+++ src/server/modules/reservation/daos/CalendarDAO.ts
@@ -1,11 +1,15 @@
-import { container, provideSingleton } from '@/di/index';
+import { container, inject, provideSingleton } from '@/di/index';
 
 import { BaseDao } from '@/modules/base/daos/BaseDao';
 import { Calendar } from '@/modules/reservation/models/Calendar';
+import { LazyServiceIdentifer } from 'inversify/lib/annotation/lazy_service_identifier';
 
 @provideSingleton()
 export class CalendarDao extends BaseDao<Calendar> {
-  private constructor(public model: Calendar) {
+  private constructor(
+    //public model: Calendar
+    @inject(new LazyServiceIdentifer(() => Calendar)) public model: Calendar,
+  ) {
     super(model);
   }
 
